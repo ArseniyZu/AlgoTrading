eea65365622e01a9c9651042dace18b1ef9d3a3d
ArseniyZu <42843555+ArseniyZu@users.noreply.github.com>
2021-04-08 00:48:12 +0300

    Update BollingerBands.py

--- BollingerBands.py
+++ BollingerBands.py
@@ -1,20 +1,20 @@
 from trend import trend
 import pandas as pd
 
-def punch_bolBands(data, interval):
+def punch_bolBands(data, interval, std):
   punch = "non"
   transdata = data[::-1].reset_index(drop=True)
   transdata = transdata[:interval]
   MSE = transdata.Close.mean()
-  up = MSE + 2 * transdata.Close.std()
-  down = MSE - 2 * transdata.Close.std()
+  up = MSE + std * transdata.Close.std()
+  down = MSE - std * transdata.Close.std()
   if transdata.High[0] > up:
     punch = "up"
   elif transdata.Low[0] < down:
     punch = "down"
   return punch
 
-def walking_bolBands(data, interval, ran):
+def walking_bolBands(data, interval, ran, std):
   count_of_up_punches = 0
   count_of_down_punches = 0
   transdata = data[::-1].reset_index(drop=True)
@@ -22,8 +22,8 @@ def walking_bolBands(data, interval, ran):
   up = []
   down = []
   for i in MSE_STD:
-    up.append(i[0] + 2 * i[1])
-    down.append(i[0] - 2 * i[1])
+    up.append(i[0] + std * i[1])
+    down.append(i[0] - std * i[1])
   for i in range(ran):
     if transdata.High[i] > up[i]:
       count_of_up_punches += 1
