ce83503080e7d425a9ce813308bfae4ed8e78272
ArseniyZu <4rseniyz@gmail.com>
2021-04-07 21:53:22 +0300

    1

---
+++ draw.py
@@ -0,0 +1,11 @@
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+def drawCandle(df, left, right):
+    fig = go.Figure(data=[go.Candlestick(x=[i for i in range(left, right + 1)],
+                open=df["Open"][left: right + 1],
+                high=df["High"][left: right + 1],
+                low=df['Low'][left: right + 1],
+                close=df['Close'][left: right + 1])])
+    fig.update_layout(title=f"df")
+    fig.show()
