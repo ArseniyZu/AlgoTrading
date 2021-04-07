604e960d0458c090353dec3515eb52be8f9ccc3d
ArseniyZu <42843555+ArseniyZu@users.noreply.github.com>
2021-04-08 00:18:36 +0300

    Update BollingerBands.py

--- BollingerBands.py
+++ BollingerBands.py
@@ -1,4 +1,3 @@
-import numpy as np
 from trend import trend
 import pandas as pd
 
@@ -6,28 +5,29 @@ def punch_bolBands(data, interval):
   punch = "non"
   transdata = data[::-1].reset_index(drop=True)
   transdata = transdata[:interval]
-  MSE = np.mean(transdata.Close)
-  up = MSE + 2 * np.std(transdata.Close)
-  down = MSE - 2 * np.std(transdata.Close)
+  MSE = transdata.Close.mean()
+  up = MSE + 2 * transdata.Close.std()
+  down = MSE - 2 * transdata.Close.std()
   if transdata.High[0] > up:
     punch = "up"
   elif transdata.Low[0] < down:
     punch = "down"
   return punch
 
-def walking_bolBands(data, interval):
+def walking_bolBands(data, interval, ran):
   count_of_up_punches = 0
   count_of_down_punches = 0
   transdata = data[::-1].reset_index(drop=True)
-  MSE_STD = [[np.mean(transdata.Close[i: interval + i]), np.std(transdata.Close[i: interval + i])] for i in range(interval)]
+  MSE_STD = [[transdata.Close[i: interval + i].mean(), transdata.Close[i: interval + i].std()] for i in range(ran)]
   up = []
   down = []
   for i in MSE_STD:
     up.append(i[0] + 2 * i[1])
     down.append(i[0] - 2 * i[1])
-  for i in range(interval):
+  for i in range(ran):
     if transdata.High[i] > up[i]:
       count_of_up_punches += 1
     if transdata.Low[i] > down[i]:
       count_of_down_punches += 1
-  return count_of_up_punches, count_of_down_punches
+  trend = trend(data, ran)[0]
+  return trend, count_of_up_punches, count_of_down_punches
