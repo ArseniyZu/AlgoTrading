5ebe673269d8717c38a7e08f8b1e1f5ad5581b70
ArseniyZu <42843555+ArseniyZu@users.noreply.github.com>
2021-04-08 00:24:02 +0300

    Update trend.py

--- trend.py
+++ trend.py
@@ -2,13 +2,14 @@ def trend(data, ran):
   trend = "non"
   trendx = []
   trendy = []
-  start = len(data) - ran
-  m1 = max(data["High"][start: start + ran // 3])
-  m2 = max(data["High"][start + ran // 3: start + ran // 3 * 2])
-  m3 = max(data["High"][start + ran // 3 * 2:])
-  min1 = min(data["Low"][start: start + ran // 3])
-  min2 = min(data["Low"][start + ran // 3: start + ran // 3 * 2])
-  min3 = min(data["Low"][start + ran // 3 * 2:])
+  transdata = data[::-1].reset_index(drop=True)
+  transdata = transdata[:ran]
+  m1 = max(transdata["High"][: ran // 3])
+  m2 = max(transdata["High"][ran // 3: ran // 3 * 2])
+  m3 = max(transdata["High"][ran // 3 * 2:])
+  min1 = min(transdata["Low"][: ran // 3])
+  min2 = min(transdata["Low"][ran // 3: ran // 3 * 2])
+  min3 = min(transdata["Low"][ran // 3 * 2:])
   if m3 > m2 > m1 or min3 > min2 > min1:
     trend = "up"
   elif m3 < m2 < m1 or min3 < min2 < min1:
