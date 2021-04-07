0e9450b3a560c60786536e5f03de1514d8b2d707
ArseniyZu <42843555+ArseniyZu@users.noreply.github.com>
2021-04-07 22:59:50 +0300

    Update trend.py

--- trend.py
+++ trend.py
@@ -1,25 +1,22 @@
-def trend_3max(data, ran):
+def trend(data, ran):
   trend = "non"
+  trendx = []
+  trendy = []
   start = len(data) - ran
   m1 = max(data["High"][start: start + ran // 3])
   m2 = max(data["High"][start + ran // 3: start + ran // 3 * 2])
   m3 = max(data["High"][start + ran // 3 * 2:])
-  if m3 > m2 > m1:
+  min1 = min(data["Low"][start: start + ran // 3])
+  min2 = min(data["Low"][start + ran // 3: start + ran // 3 * 2])
+  min3 = min(data["Low"][start + ran // 3 * 2:])
+  if m3 > m2 > m1 or min3 > min2 > min1:
     trend = "up"
-  elif m3 < m2 < m1:
+  elif m3 < m2 < m1 or min3 < min2 < min1:
     trend = "down"
-  return trend
-
-
-def trend_3min(data, ran):
-  trend = "non"
-  start = len(data) - ran
-  m1 = min(data["Low"][start: start + ran // 3])
-  m2 = min(data["Low"][start + ran // 3: start + ran // 3 * 2])
-  m3 = min(data["Low"][start + ran // 3 * 2:])
-  if m3 > m2 > m1:
-    trend = "up"
-  elif m3 < m2 < m1:
-    trend = "down"
-  return trend
-  
+  if trend == "down":
+    trendy.append([m1, m3])
+    trendx.append([start, len(data)])
+  elif trend == "up":
+    trendy.append([min1, min3])
+    trendx.append([start, len(data)])
+  return trendx, trendy
