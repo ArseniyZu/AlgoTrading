a15139f54cb8d1e1c54c64c6b77fd4b0da9d3ff2
ArseniyZu <42843555+ArseniyZu@users.noreply.github.com>
2021-04-08 17:03:48 +0300

    Update figuresBolBands.py

--- figuresBolBands.py
+++ figuresBolBands.py
@@ -6,7 +6,7 @@ def W_figure_criterion(right_max, left_max, middle_max, right_min, left_min):
   if right_max > right_min and right_max > left_min:
     if left_max > right_min and left_max > left_min:
       if middle_max > left_min and middle_max > right_min:
-        if middle_max < right_max / 2 and middle_max < left_max / 2:
+        if middle_max < right_max and middle_max < left_max:
           check = True
   return check
 
@@ -14,13 +14,13 @@ def find_W_figure(data, interval):
   W = "non"
   transdata = data[::-1].reset_index(drop=True)
   transdata = transdata[: interval]
-  right_max = transdata.Low[0]
-  left_max = transdata.Low[interval - 1]
-  right_min_idx = transdata.Low[: interval // 2].idxmin()
-  left_min_idx  = transdata.Low[interval // 2 + 1: interval].idxmin()
-  right_min = min(transdata.Low[: interval // 2])
-  left_min = min(transdata.Low[interval // 2 + 1: interval])
-  middle_max = max(transdata.Low[right_min_idx + 1: left_min_idx])
+  right_max = transdata.High[0]
+  left_max = transdata.High[interval - 1]
+  right_min_idx = transdata.High[: interval // 2].idxmin()
+  left_min_idx  = transdata.High[interval // 2 + 1: interval].idxmin()
+  right_min = min(transdata.High[: interval // 2])
+  left_min = min(transdata.High[interval // 2 + 1: interval])
+  middle_max = max(transdata.High[right_min_idx + 1: left_min_idx])
   if W_figure_criterion(right_max, left_max, middle_max, right_min, left_min):
     if punch_bolBands(data[: len(data) - left_min_idx], 20, 3):
       W = "yes"
