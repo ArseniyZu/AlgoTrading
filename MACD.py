a79e57999ac5b29715a3c20ce905a780d02a2fa4
ArseniyZu <42843555+ArseniyZu@users.noreply.github.com>
2021-04-14 14:34:45 +0300

    Update MACD.py

--- MACD.py
+++ MACD.py
@@ -5,7 +5,7 @@ def MACD(data, len1, len2, siglen):
   data['MACD'] = data.Close
   transdata = data[::-1].reset_index(drop=True)
   for i in range(len(transdata.Close) - len2):
-    transdata["MACD"][i] = np.mean(trasndata.Close[i:i + len1]) - np.mean(transdata.Close[i: i + len2])
+    transdata["MACD"][i] = np.mean(transdata.Close[i:i + len1]) - np.mean(transdata.Close[i: i + len2])
   for i in range(len(transdata["MACD"]) - siglen):
     transdata["Signal"][i] = np.mean(transdata["MACD"][i:i + siglen])
   data = transdata[::-1].reset_index(drop=True)
