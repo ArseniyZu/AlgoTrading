2dd8f6434f5744b9e471a3ac67fbcf65b68c1008
ArseniyZu <42843555+ArseniyZu@users.noreply.github.com>
2021-04-14 14:35:41 +0300

    Update MACD.py

--- MACD.py
+++ MACD.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def MACD(data, len1, len2, siglen):
   data['MACD'] = data.Close
+  data["Signal"] = data.Close
   transdata = data[::-1].reset_index(drop=True)
   for i in range(len(transdata.Close) - len2):
     transdata["MACD"][i] = np.mean(transdata.Close[i:i + len1]) - np.mean(transdata.Close[i: i + len2])
