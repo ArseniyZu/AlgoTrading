7352acff455532961d8683275808f1a0230dbfe5
ArseniyZu <4rseniyz@gmail.com>
2021-04-15 19:01:21 +0300

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,47 @@
+
+class Operations():
+
+    def __init__(self, budget):
+        self.budget = budget
+        self.assets = {}
+        self.buy_price = {}
+
+    def sell(self, name, count, price):
+        if name in self.assets.keys():
+            if self.assets[name] == 0:
+                print("No asset in estate")
+            if self.assets[name] >= count:
+                self.assets[name] -= count
+                self.budget += count * price
+            else:
+                print("Selling counts more than having")
+        else:
+            print("No asset of this ticker")
+
+    def buy(self, name, count, price):
+        if self.budget >= price * count:
+            self.budget -= price * count
+            if name in self.assets.keys():
+                self.assets[name] += count
+                self.buy_price[name] = price
+            else:
+                self.buy_price.update({name: price})
+                self.assets.update({name: count})
+        else:
+            print("No money in budget")
+
+    def stop_loss(self, name, current_price, percentage):
+        if (self.buy_price[name] - current_price) / self.buy_price[name] > percentage:
+            self.sell(name, self.assets[name], current_price)
+
+    def take_profit(self, name, current_price, percentage):
+        if (current_price - self.buy_price[name]) / self.buy_price[name] > percentage:
+            self.sell(name, self.assets[name], current_price)
+
+
+a = Operations(1000)
+a.buy("MTC", 5, 100)
+a.stop_loss("MTC", 90, 0.05)
+print(a.budget, a.assets)
+a.take_profit("MTC", 110, 0.05)
+print(a.budget, a.assets)
